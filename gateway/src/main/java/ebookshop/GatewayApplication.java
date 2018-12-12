e9d629f3dc2f7c2f654edfb8818bdbe1b6c5404c
Ka Yi <chua.kayi@yahoo.com.sg>
2018-12-12 13:44:04 +0800

    Change gateway route info to use Java, not yml

--- gateway/src/main/java/ebookshop/GatewayApplication.java
+++ gateway/src/main/java/ebookshop/GatewayApplication.java
@@ -3,6 +3,9 @@ package ebookshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,6 +14,18 @@ public class GatewayApplication {
     public static void main(String[] args) {
     	System.setProperty("spring.config.name", "gateway-server");
         SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/api/books/**")
+						.uri("lb://books-service"))
+				.route(p -> p
+						.path("/books/**")
+						.uri("lb://web-service"))
+				.build();
     }
 
 }
